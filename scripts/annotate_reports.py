9d6c9a1c35c4f4afed40c7c7efc294d89a8e498f
freinque <freinque.prof@gmail.com>
2025-03-15 11:39:27 -0400

    basic clean of annotate_reports.py file

--- scripts/annotate_reports.py
+++ scripts/annotate_reports.py
@@ -9,6 +9,20 @@ import click
 from xray_report_gen import report_annotation
 from xray_report_gen.config import DATA_PATH, PROMPT_VERSIONS, BEST_PROMPT_VERSION, N
 
+def process_split(df_split, split_name):
+    reports_split = df_split['original_report']
+    if split_name == 'val':
+        prompt_versions = [BEST_PROMPT_VERSION]
+    else:
+        prompt_versions = PROMPT_VERSIONS
+
+    for prompt_version in prompt_versions:
+        reports_annotations = report_annotation.annotate_reports(reports_split, prompt_version=prompt_version)
+        df_split['annotation_{prompt_version}'.format(prompt_version=prompt_version)] = reports_annotations
+
+    df_split.to_csv(os.path.join(DATA_PATH, f'reports_annotations_{split_name}.csv'), index=False)
+
+
 @click.command()
 @click.argument('mode', type=str, default='train')
 def main(mode):
@@ -18,38 +32,14 @@ def main(mode):
     df_test = df[df['split'] == 'test'].sample(n=N)
     df_val = df[df['split'] == 'val']
 
-    reports_train = df_train['original_report']
-    reports_test = df_test['original_report']
-    reports_val = df_val['original_report']
-
     if mode == 'train':
-        # looking at results on df_train
-        for prompt_version in PROMPT_VERSIONS:
-            # processing df_train with various prompt variants
-            reports_annotations_train = report_annotation.annotate_reports(reports_train, prompt_version=prompt_version)
-            df_train['annotation_{prompt_version}'.format(prompt_version=prompt_version)] = reports_annotations_train
-
-        # write to data storage
-        df_train.to_csv(os.path.join(DATA_PATH, 'reports_annotations_train.csv'), index=False)
+        process_split(df_train, 'train')
 
     if mode == 'test':
-        # looking at results on df_test
-        for prompt_version in PROMPT_VERSIONS:
-            # processing df_test with various prompt variants
-            reports_annotations_test = report_annotation.annotate_reports(reports_test, prompt_version=prompt_version)
-            df_test['annotation_{prompt_version}'.format(prompt_version=prompt_version)] = reports_annotations_test
-
-        # write to data storage
-        df_test.to_csv(os.path.join(DATA_PATH, 'reports_annotations_test.csv'), index=False)
+        process_split(df_test, 'test')
 
     if mode == 'val':
-        # processing df_val with various prompt variants
-        prompt_version = BEST_PROMPT_VERSION
-        reports_annotations_val = report_annotation.annotate_reports(reports_val, prompt_version=prompt_version)
-        df_val['annotation_{prompt_version}'.format(prompt_version=prompt_version)] = reports_annotations_val
-
-        # write to data storage
-        df_val.to_csv(os.path.join(DATA_PATH, 'reports_annotations_val.csv'), index=False)
+        process_split(df_val, 'val')
 
 if __name__ == '__main__':
     main()
