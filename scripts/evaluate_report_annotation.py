f9beecfcb9ef534905121d7ac58ba504a6224623
freinque <freinque.prof@gmail.com>
2025-03-16 11:55:59 -0400

    ran report annotation part

--- scripts/evaluate_report_annotation.py
+++ scripts/evaluate_report_annotation.py
@@ -14,51 +14,51 @@ from xray_report_gen import utils
 from xray_report_gen.config import DATA_PATH, REPORT_ANNOTATION_PROMPT_VERSIONS, REGIONS
 
 @click.command()
-@click.argument('mode')
+@click.argument('split')
 @click.option('--multi', type=int, default=0)
-def main(mode, multi):
+def main(split, multi):
     # evaluating on the training set
-    print('mode:', mode)
+    print('split:', split)
 
     if multi == 0:
         # evaluation on train set
         for prompt_version in REPORT_ANNOTATION_PROMPT_VERSIONS:
             for region in REGIONS:
                 # load the processed data from storage
-                df_mode = pd.read_csv(os.path.join(DATA_PATH, 'reports_annotations_{mode}.csv'.format(mode=mode)))
-                df_mode = utils.parse_annotations(df_mode, 'annotation', REGIONS)
+                df_split = pd.read_csv(os.path.join(DATA_PATH, 'reports_annotations_{split}.csv'.format(split=split)))
+                df_split = utils.parse_annotations(df_split, 'annotation', REGIONS)
 
-                df_mode = utils.parse_annotations(df_mode, 'annotation_{prompt_version}'.format(prompt_version=prompt_version), REGIONS)
+                df_split = utils.parse_annotations(df_split, 'annotation_{prompt_version}'.format(prompt_version=prompt_version), REGIONS)
 
                 print('evaluating on {prompt_version} and {region}'.format(prompt_version=prompt_version, region=region))
-                ref = df_mode['annotation_{region}'.format(region=region)]
-                hyp = df_mode['annotation_{prompt_version}_{region}'.format(prompt_version=prompt_version, region=region)]
+                ref = df_split['annotation_{region}'.format(region=region)]
+                hyp = df_split['annotation_{prompt_version}_{region}'.format(prompt_version=prompt_version, region=region)]
                 print('ref = {ref}'.format(ref=ref))
                 print('hyp = {hyp}'.format(hyp=hyp))
                 res_df = eval.get_green_scorer_res(ref, hyp)
-                res_df.to_csv(os.path.join(DATA_PATH, 'reports_annotations_{mode}_eval_{prompt_version}_{region}.csv'.format(mode=mode, prompt_version=prompt_version, region=region)), index=False)
+                res_df.to_csv(os.path.join(DATA_PATH, 'reports_annotations_{split}_eval_{prompt_version}_{region}.csv'.format(split=split, prompt_version=prompt_version, region=region)), index=False)
     else:
         # evaluation on train set
         for prompt_version in REPORT_ANNOTATION_PROMPT_VERSIONS:
             for region in REGIONS:
                 # load the processed data from storage
-                df_mode = pd.read_csv(os.path.join(DATA_PATH,
-                                                   'rep_im_annotations_{mode}_model_{prompt_version}.csv'.format(
-                                                       mode=mode, prompt_version=prompt_version)))
+                df_split = pd.read_csv(os.path.join(DATA_PATH,
+                                                   'rep_im_annotations_{split}_model_{prompt_version}.csv'.format(
+                                                       split=split, prompt_version=prompt_version)))
                 df = pd.read_csv(os.path.join(DATA_PATH, 'data_prep.csv'))[['id','annotation']].drop_duplicates()
-                df_mode = pd.merge(df_mode, df, how='left', on='id')
+                df_split = pd.merge(df_split, df, how='left', on='id')
 
-                df_mode = utils.parse_annotations(df_mode, 'annotation', REGIONS)
+                df_split = utils.parse_annotations(df_split, 'annotation', REGIONS)
 
-                df_mode = utils.parse_annotations(df_mode, 'annotation_{prompt_version}'.format(prompt_version=prompt_version), REGIONS)
+                df_split = utils.parse_annotations(df_split, 'annotation_{prompt_version}'.format(prompt_version=prompt_version), REGIONS)
 
                 print('evaluating mutimodal on {prompt_version} and {region}'.format(prompt_version=prompt_version, region=region))
-                ref = df_mode['annotation_{region}'.format(region=region)]
-                hyp = df_mode['annotation_{prompt_version}_{region}'.format(prompt_version=prompt_version, region=region)]
+                ref = df_split['annotation_{region}'.format(region=region)]
+                hyp = df_split['annotation_{prompt_version}_{region}'.format(prompt_version=prompt_version, region=region)]
                 print('ref = {ref}'.format(ref=ref))
                 print('hyp = {hyp}'.format(hyp=hyp))
                 res_df = eval.get_green_scorer_res(ref, hyp)
-                res_df.to_csv(os.path.join(DATA_PATH, 'rep_im_annotations_{mode}_eval_{prompt_version}_{region}.csv'.format(mode=mode, prompt_version=prompt_version, region=region)), index=False)
+                res_df.to_csv(os.path.join(DATA_PATH, 'rep_im_annotations_{split}_eval_{prompt_version}_{region}.csv'.format(split=split, prompt_version=prompt_version, region=region)), index=False)
 
 if __name__ == '__main__':
     main()
